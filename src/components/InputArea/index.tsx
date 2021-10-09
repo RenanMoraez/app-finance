334238e5ce1751b8c561ef023d115ca355c7fe1a
Renan Moraez <renanmoraesaraujo@gmail.com>
2021-10-09 10:34:48 -0300

    concluindo o projeto do app-finance, existe algumas falhas, porém é executavel

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import * as C from './styles';
 import { Item } from '../../types/Items';
+
+
+import { categories } from '../../data/categories';
+
 
 type Props = {
     onAdd: (item: Item) => void;
@@ -8,21 +13,87 @@ type Props = {
 
 export const InputArea = ({ onAdd }: Props) => {
 
-    const handleAddEvent = () => {
-        let newItem: Item = {
-            date: new Date(2021, 2, 22),
-            category: 'food', 
-            title: 'Item de teste',
-            value: 240.23
+    const [dateField, setDateField] = useState('');
+    const [categoryField, setCategoryField] = useState('');
+    const [titleField, setTitleField] = useState('');
+    const [valueField, setValueField] = useState(0);
 
+
+    let categoryKeys: string[] = Object.keys(categories);
+
+    const handleAddEvent = () => {
+        let errors: string[] = [];
+
+        if (isNaN(new Date(dateField).getTime())) {
+            errors.push('Data Inválida');
         }
-        
-        onAdd(newItem);
+
+        if (!categoryKeys.includes(categoryField)) {
+            errors.push('Categoria Inválida');
+        }
+
+        if (titleField === '') {
+            errors.push('Título Vazio!')
+        }
+
+        if (valueField <= 0) {
+            errors.push('Valor inválido');
+        }
+
+        if (errors.length > 0) {
+            alert(errors.join("\n"));
+        } else {
+            onAdd({
+                date: new Date(dateField),
+                category: categoryField,
+                title: titleField,
+                value: valueField
+            });
+            clearFields();
+        }
     }
 
-    return(
+    const clearFields = () => {
+        setDateField('');
+        setCategoryField('');
+        setTitleField('');
+        setValueField(0);
+    }
+
+    return (
         <C.Container>
-            <button onClick={handleAddEvent}>Adicionar</button>
+            <C.InputLabel>
+                <C.InputTitle>Data</C.InputTitle>
+                    <C.Input type="date" value={dateField} onChange={e=> setDateField(e.target.value)} />
+            </C.InputLabel>
+
+            <C.InputLabel>
+            <C.InputTitle>Categoria</C.InputTitle>
+                <C.Select value={categoryField} onChange={e => setCategoryField(e.target.value)}>
+                    <>
+                    <option></option>
+                    {categoryKeys.map((key, index) => (
+                        <option key={index} value={key}>{categories[key].title}</option>
+                    ))}
+                    </>
+                </C.Select>
+            </C.InputLabel>
+
+            <C.InputLabel>
+            <C.InputTitle>Título</C.InputTitle>
+                   <C.Input type="text" value={titleField} onChange={e => setTitleField(e.target.value)} />     
+            </C.InputLabel>
+
+            <C.InputLabel>
+            <C.InputTitle>Valor</C.InputTitle>
+                   <C.Input type="number" value={valueField} onChange={e => setValueField(parseFloat(e.target.value))} />     
+            </C.InputLabel>
+
+            <C.InputLabel>
+                <C.InputTitle>&nbsp;</C.InputTitle>
+                <C.Button onClick={handleAddEvent}>Adicionar</C.Button>
+            </C.InputLabel>
+
         </C.Container>
     );
 }
--- src/components/InputArea/styles.ts
+++ src/components/InputArea/styles.ts
@@ -10,3 +10,49 @@ export const Container = styled.div`
 
 
 `;
+
+export const InputLabel = styled.label`
+    flex:1;
+    margin: 19px;
+`;
+
+export const InputTitle = styled.div`
+    font-weight: bold;
+    margin-bottom: 5px;
+
+`;
+
+export const Input = styled.input`
+
+    width: 100%;
+    height: 30px;
+    padding: 0 5px;
+    border: 1px solid #e74c3c;
+    border-radius: 5px;
+`;
+
+
+export const Select = styled.select`
+    width: 100%;
+    height: 30px;
+    padding: 0 5px;
+    border: 1px solid #e74c3c;
+    border-radius: 5px;
+`;
+
+
+export const Button = styled.button`
+    width: 100%;
+    height: 30px;
+    padding: 0 5px;
+    border: 1px solid #e74c3c;
+    border-radius: 5px;
+    background-color: #e74c3c;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #c0392b;
+        color: #fff;
+    }
+`;
